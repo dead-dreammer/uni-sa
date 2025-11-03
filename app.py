202a93b84d209ac5d1b25b1ee34a8cc590a624db
dead-dreammer <dalziel.sarkhot94@gmail.com>
2025-11-03 14:35:34 +0200

    adding reports to matching page

--- app.py
+++ app.py
@@ -15,14 +15,6 @@ from functools import wraps
 
 app = create_app()
 
-@app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
-
-
 # --- Routes for template pages ---
 @app.route('/')
 @app.route('/home')
